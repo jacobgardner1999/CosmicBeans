f5407875d78083395376a6aa8bd36ab41f29289e
Jacob Gardner <Jacob.gardner@bjss.com>
2023-07-30 16:54:03 +0100

    chore: adding Javafx dependency to maven

---
+++ src/main/java/UI/Main.java
@@ -0,0 +1,12 @@
+package UI;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
