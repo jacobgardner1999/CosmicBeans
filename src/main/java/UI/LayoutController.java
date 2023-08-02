d80f73c29bb2fd39c9773017ea31ddb160ab6a8e
Jacob Gardner <Jacob.gardner@bjss.com>
2023-08-02 15:19:09 +0100

    refactor: completed UI refactor to use fxml file and controller class

--- src/main/java/UI/LayoutController.java
+++ src/main/java/UI/LayoutController.java
@@ -1,40 +1,28 @@
-package FXMLUI;
+package UI;
 
+import Components.Option;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import Components.Player;
+
+import java.util.Objects;
 
 public class LayoutController {
     private final StringProperty dynamicResultProperty = new SimpleStringProperty("Default Text");
     private String dynamicResult = "";
     private Timeline timeline;
     private final Player player;
-    private String previousText;
 
     public LayoutController(Player player) {
         this.player = player;
-        previousText = player.getCurrentChoiceText();
-        Thread changeDetectionThread = new Thread(() -> {
-            String currentText = player.getCurrentChoiceText();
-            if (!currentText.equals(previousText)) {
-                updateText(currentText);
-                previousText = currentText;
-            }
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        changeDetectionThread.setDaemon(true);
-        changeDetectionThread.start();
     }
 
     @FXML
@@ -45,13 +33,36 @@ public class LayoutController {
     @FXML
     private void initialize() {
         mainText.textProperty().bind(dynamicResultProperty);
+        updateText(player.getCurrentChoiceText());
+        generateButtons();
     }
 
     public void updateText(String newText) {
         javafx.application.Platform.runLater(() -> {
             dynamicResult = newText;
             startTypingAnimation();
+            generateButtons();
         });
+    }
+
+    private void generateButtons() {
+        buttonContainer.getChildren().clear();
+        for (Option option : player.getCurrentChoice().getOptionsList()) {
+            Button button = new Button(option.getOptionText());
+            buttonContainer.getChildren().add(button);
+
+            button.setOnAction(event -> {
+                int i = 0;
+                int optionIndex = 404;
+                for (Option o : player.getCurrentChoice().getOptionsList()) {
+                    if (Objects.equals(o.getOptionText(), button.getText())) {
+                        optionIndex = i;
+                    }
+                    i += 1;
+                }
+                player.makeChoice(optionIndex);
+            });
+        }
     }
 
     private void startTypingAnimation() {
--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -1,83 +1,34 @@
 package UI;
+
 import Components.Game;
-import Components.Option;
 import Components.Player;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.util.Objects;
 
+public class Main extends Application {
 
-public class Main extends Application{
-    private final StringProperty dynamicResultProperty = new SimpleStringProperty();
-    private String dynamicResult = "";
-    private Timeline timeline;
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
-    public void start(Stage primaryStage) {
-        Player player = new Player();
+    public void start(Stage primaryStage) throws Exception {
         Game game = new Game();
+        Player player = new Player();
 
         game.setupGame(player);
 
-        Text mainText = new Text();
-        mainText.textProperty().bind(dynamicResultProperty);
-        dynamicResultProperty.set(dynamicResult);
-        mainText.setFill(Color.WHITE);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/layout.fxml"));
+        fxmlLoader.setControllerFactory(param -> new LayoutController(player));
+        Parent root = fxmlLoader.load();
 
-        VBox buttonBox = new VBox();
-        buttonBox.setAlignment(javafx.geometry.Pos.CENTER);
-        buttonBox.setSpacing(10);
+        LayoutController controller = fxmlLoader.getController();
 
-        Runnable generateButtons = () -> {
-            buttonBox.getChildren().clear();
-            for (Option option : player.getCurrentChoice().getOptionsList()) {
-                Button button = new Button(option.getOptionText());
-                buttonBox.getChildren().add(button);
-
-                button.setOnAction(event -> {
-                    int i = 0;
-                    int optionIndex = 404;
-                    for(Option o : player.getCurrentChoice().getOptionsList()) {
-                        if (Objects.equals(o.getOptionText(), button.getText())) {
-                            optionIndex = i;
-                        }
-                        i += 1;
-                    }
-                    player.makeChoice(optionIndex);
-                });
-            }
-        };
-
-        generateButtons.run();
-
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(mainText, buttonBox);
-
-        stackPane.setStyle("-fx-background-color: black;");
-
-        StackPane.setAlignment(stackPane, javafx.geometry.Pos.TOP_CENTER);
-
-        StackPane.setMargin(mainText, new Insets(-40,0,0,0));
-        StackPane.setMargin(buttonBox, new Insets(40, 0, 0, 0));
-
-        Scene scene = new Scene(stackPane, 400, 300);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Cosmic Beans Dev Test");
+        primaryStage.setTitle("Cosmic Beans DEV TEST");
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
 
         new Thread(() -> {
@@ -85,8 +36,7 @@ public class Main extends Application{
                 while (true) {
                     String newText = player.getCurrentChoiceText();
 
-                    updateTextFromThread(newText);
-                    javafx.application.Platform.runLater(generateButtons);
+                    controller.updateText(newText);
 
                     Thread.sleep(1000);
                 }
@@ -94,34 +44,5 @@ public class Main extends Application{
                 e.printStackTrace();
             }
         }).start();
-    }
-
-    private void updateTextFromThread(String newText) {
-        javafx.application.Platform.runLater(() -> {
-            dynamicResult = newText;
-            startTypingAnimation();
-        });
-    }
-    private void startTypingAnimation() {
-        if (timeline != null && timeline.getStatus() != Animation.Status.STOPPED || dynamicResultProperty.get().equals(dynamicResult)) {
-            return;
-        }
-
-        timeline = new Timeline();
-
-        Duration frameDuration = Duration.millis(100);
-
-        for (int i = 0; i <= dynamicResult.length(); i++) {
-            int finalI = i;
-            KeyFrame keyFrame = new KeyFrame(frameDuration.multiply(i), event -> dynamicResultProperty.set(dynamicResult.substring(0, finalI)));
-            timeline.getKeyFrames().add(keyFrame);
-        }
-
-        KeyFrame lastKeyFrame = new KeyFrame(frameDuration.multiply(dynamicResult.length() + 1), event -> dynamicResultProperty.set(dynamicResult));
-        timeline.getKeyFrames().add(lastKeyFrame);
-
-        timeline.setCycleCount(1);
-
-        timeline.play();
     }
 }
